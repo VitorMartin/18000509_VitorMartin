2ea9efd73d3f7b10ec0e64ca473b8297deb23020
VitorMartin <vitor.martin.simoni@gmail.com>
2020-04-27 13:50:15 -0300

    Prativa07 - parte 2

--- Praticas/Pratica07/Cachorro.java
+++ Praticas/Pratica07/Cachorro.java
@@ -1,10 +1,13 @@
-public class Cachorro extends Animal {
+public class Cachorro extends Canino  {
     @Override
     public void comer() {
         System.out.println("Au au!");
     }
 
+    @Override
     public void cheirar(){
         System.out.println("Sniff");
     }
+
+
 }
