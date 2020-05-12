db276e87dd35b210b301cd243526e62badb7fd90
VitorMartin <vitor.martin.simoni@gmail.com>
2020-05-12 09:19:24 -0300

    Listas

---
+++ Praticas/Pratica_07/Projeto_Heranca_Poly/src/com/company/Lobo.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Lobo extends Canino {
+    @Override
+    public void comer() {
+        System.out.println("Chapeuzinho!");
+    }
+
+    @Override
+    public void cheirar(){
+        System.out.println("Vishhh");
+    }
+}
