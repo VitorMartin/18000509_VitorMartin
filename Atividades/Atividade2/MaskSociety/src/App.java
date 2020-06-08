6f9879bd34a0466abaa2d76c15ea31381e37ffec
VitorMartin <vitor.martin.simoni@gmail.com>
2020-06-08 13:03:50 -0300

    Pronto: cadastrar membro e trocar jornada

--- Atividades/Atividade2/MaskSociety/src/App.java
+++ Atividades/Atividade2/MaskSociety/src/App.java
@@ -1,18 +1,55 @@
-import Menu.Menu;
-
 //Daniel Ughini Xavier RA:18.00022-3
 //Vitor Martin Simoni RA:18.00050-9
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import Enums.Horarios;
+import Funcionario.Funcionario;
+import Interfaces.BigBrother;
+import Interfaces.HeavyLifter;
+import Interfaces.MobileMember;
+import Interfaces.ScriptGuys;
+import Menu.Menu;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Menu menu = new Menu();
-        int inp;
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Funcionario> funcionarios = new ArrayList();
+        Horarios horario = Horarios.NORMAL;
+        int inp1;
         do {
-            inp = menu.getInp();
-            switch (inp) {
+            inp1 = menu.getInitialInp();
+            switch (inp1) {
                 case 1:
                     // Cadastrar novo membro
+                    String cadastro[] = new String[3];
+
+                    cadastro = menu.cadastrarMembro();
+                    String nick = cadastro[0], email = cadastro[1];
+                    int func = Integer.parseInt(cadastro[2]);
+
+                    switch (func) {
+                        case 1:
+                            funcionarios.add(new BigBrother(nick, email));
+                            break;
+
+                        case 2:
+                            funcionarios.add(new HeavyLifter(nick, email));
+                            break;
+                        
+                        case 3:
+                            funcionarios.add(new MobileMember(nick, email));
+                            break;
+
+                        case 4:
+                            funcionarios.add(new ScriptGuys(nick, email));
+                            break;
+                    
+                        default:
+                            break;
+                    }
                     break;
 
                 case 2:
@@ -21,22 +58,31 @@ public class App {
 
                 case 3:
                     // Remover colaborador
+
                     break;
 
                 case 4:
                     // Trocar jornada de trabalho
+                    int inp2 = menu.mudarJornada();
+                    if (inp2 == 1)
+                        horario = Horarios.NORMAL;
+                    else if (inp2 == 2)
+                        horario = Horarios.EXTRA;
                     break;
 
                 case 5:
                     // Postar mensagens
-
+                    
                 case 9:
                     // Sair
+
                     break;
-            
+
                 default:
                     break;
             }
-        } while (inp != 9);
+            System.out.println("Aperte ENTER para continuar");
+            scanner.nextLine();
+        } while (inp1 != 9);
     }
 }
