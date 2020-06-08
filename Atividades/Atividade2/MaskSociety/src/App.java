ef8454c334bd9b61c252bc0f5f500020a2cc6e37
VitorMartin <vitor.martin.simoni@gmail.com>
2020-06-08 10:41:43 -0300

    Inserindo os nomes da dupla

--- Atividades/Atividade2/MaskSociety/src/App.java
+++ Atividades/Atividade2/MaskSociety/src/App.java
@@ -1,3 +1,7 @@
+//Daniel Ughini Xavier RA:18.00022-3
+//Vitor Martin Simoni RA:18.00050-9
+
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
