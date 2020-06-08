d3e1b717bf231c7f0a7ac560d73185f8c0c55746
VitorMartin <vitor.martin.simoni@gmail.com>
2020-06-08 13:57:11 -0300

    implementando interfaces

--- Atividades/Atividade2/MaskSociety/src/Funcionario/Funcionario.java
+++ Atividades/Atividade2/MaskSociety/src/Funcionario/Funcionario.java
@@ -1,6 +1,9 @@
 package Funcionario;
 
-public class Funcionario {
+import Interfaces.Apresentacoes;
+import Interfaces.PostarMensagens;
+
+public class Funcionario implements PostarMensagens, Apresentacoes{
     public Funcionario(String nick, String email, String funcao) {
         this.nick = nick;
         this.email = email;
@@ -17,7 +20,14 @@ public class Funcionario {
         return email;
     }
 
+    @Override
     public void show() {
         System.out.println("Nome: " + nick + "\tEmail: " + email + "\tFuncao: " + funcao);
     }
+
+    @Override
+    public void msgExtra() {}
+
+    @Override
+    public void msgNormal() {}
 }
