b66ab0a84e139642ecc2e0fe07325c417bb590b9
VitorMartin <vitor.martin.simoni@gmail.com>
2020-06-08 11:32:57 -0300

    Extraindo input do usuario

--- Atividades/Atividade2/MaskSociety/src/App.java
+++ Atividades/Atividade2/MaskSociety/src/App.java
@@ -1,9 +1,42 @@
+import Menu.Menu;
+
 //Daniel Ughini Xavier RA:18.00022-3
 //Vitor Martin Simoni RA:18.00050-9
 
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Menu menu = new Menu();
+        int inp;
+        do {
+            inp = menu.getInp();
+            switch (inp) {
+                case 1:
+                    // Cadastrar novo membro
+                    break;
+
+                case 2:
+                    // Apresentar membros
+                    break;
+
+                case 3:
+                    // Remover colaborador
+                    break;
+
+                case 4:
+                    // Trocar jornada de trabalho
+                    break;
+
+                case 5:
+                    // Postar mensagens
+
+                case 9:
+                    // Sair
+                    break;
+            
+                default:
+                    break;
+            }
+        } while (inp != 9);
     }
 }
--- Atividades/Atividade2/MaskSociety/src/Menu/Menu.java
+++ Atividades/Atividade2/MaskSociety/src/Menu/Menu.java
@@ -1,5 +1,20 @@
 package Menu;
 
+import java.util.Scanner;
+
 public class Menu {
+    private Scanner scanner = new Scanner(System.in);
     
+    public int getInp(){
+        System.out.println(
+            "\n" +
+            "1 - Cadastrar novo membro\n" + 
+            "2 - Apresentar membros cadastrados\n" + 
+            "3 - Remover colaborador\n" + 
+            "4 - Trocar jornada de trabalho\n" + 
+            "5 - Postar mensagens\n" + 
+            "9 - Sair"
+        );
+        return scanner.nextInt();
+    }
 }
