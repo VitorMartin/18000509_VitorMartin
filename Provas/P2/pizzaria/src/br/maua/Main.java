a59116192a687b7f2e62f7d1ca1e9b50795b6d38
VitorMartin <vitor.martin.simoni@gmail.com>
2020-06-24 14:33:09 -0300

    Criando metodo para o scanner.nextLine()

--- Provas/P2/pizzaria/src/br/maua/Main.java
+++ Provas/P2/pizzaria/src/br/maua/Main.java
@@ -1,8 +1,24 @@
 package br.maua;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String a = input("str a: ");
+        String b = input("str b: ");
+
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+
+    private static String input(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(prompt);
+        String inp = scanner.nextLine();
+//        scanner.close(); // Nao sei pq, mas se eu fechar o scanner, so posso chamar essa funcao UMA vez.
+        return inp;
     }
 }
