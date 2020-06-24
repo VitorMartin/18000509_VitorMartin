d38d009cb276548d266ae4961125b1039ac9c286
VitorMartin <vitor.martin.simoni@gmail.com>
2020-06-24 13:48:57 -0300

    Criando classe Usuario.java

---
+++ Provas/P2/pizzaria/src/br/maua/funcionario/Usuario.java
@@ -0,0 +1,11 @@
+package br.maua.funcionario;
+
+public class Usuario {
+    private String nome, email, senha;
+
+    public Usuario(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+}
