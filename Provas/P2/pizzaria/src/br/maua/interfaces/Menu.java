e1cb6092b3d8e75a4c402c0c4fb425b976aa3b53
VitorMartin <vitor.martin.simoni@gmail.com>
2020-06-25 12:38:27 -0300

    javadoc da interface Menu

--- Provas/P2/pizzaria/src/br/maua/interfaces/Menu.java
+++ Provas/P2/pizzaria/src/br/maua/interfaces/Menu.java
@@ -7,14 +7,30 @@ import br.maua.maps.Pagamentos;
 
 import java.util.ArrayList;
 
+/**
+ * Interface para o menu. Responsável por apresentar todos os menus e opções ao usuário.
+ * Essa classe apenas imprime conteúdos na tela, pois quem é responsável por coletar as informações
+ * que o usuário digita é a classe Main
+ *
+ * @see br.maua.Main
+ */
+
 public interface Menu {
 
+    /**
+     * Menu de login do usuário
+     */
     static void login(){
         System.out.println(
             "\nFaca seu login:"
         );
     }
 
+    /**
+     * Menu de saudação ao usuário. Também diz se ele é administrador ou não.
+     * @param usr Instância de Usuario.
+     * @see Usuario
+     */
     static void saudacao(Usuario usr){
         // Template de como essa string deve ficar:
         /*
@@ -28,20 +44,37 @@ public interface Menu {
         System.out.println(str);
     }
 
+    /**
+     * Despedida do usuário.
+     */
     static void despedida(){
         System.out.println("Ate mais!");
     }
 
+    /**
+     * Opçoes de pagamento.
+     *
+     * @see Pagamentos
+     */
     static void opcoesDePagamento(){
         System.out.println("Formas de pagamento:");
         System.out.println(Pagamentos.mapToString());
     }
 
+    /**
+     * Opções do estado de um pedido.
+     *
+     * @see Estados
+     */
     static void opcoesDeEstado() {
         System.out.println("Estados possiveis:");
         System.out.println(Estados.mapToString());
     }
 
+    /**
+     * Mostra a lista de pedidos criados.
+     * @param pedidos ArrayList que guarda todos os pedidos criados.
+     */
     static void mostrarPedidos(ArrayList<Pedido> pedidos) {
         StringBuilder str = new StringBuilder();
         for (Pedido p : pedidos) {
@@ -50,6 +83,9 @@ public interface Menu {
         System.out.println(str);
     }
 
+    /**
+     * Mostra o menu principal.
+     */
     static void menuPrincipal() {
         System.out.println(
             "\nPizzaria o Rato que Ri:" + '\n' +
@@ -59,6 +95,5 @@ public interface Menu {
             "9 - Sair"
         );
     }
-
 
 }
