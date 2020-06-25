49edbc5721d28ade38a26cd17546a2dbe0e4ab12
VitorMartin <vitor.martin.simoni@gmail.com>
2020-06-25 12:38:38 -0300

    javadoc da interface Seguranca

--- Provas/P2/pizzaria/src/br/maua/interfaces/Seguranca.java
+++ Provas/P2/pizzaria/src/br/maua/interfaces/Seguranca.java
@@ -1,7 +1,18 @@
 package br.maua.interfaces;
 
+/**
+ * Interface responsável por autenticar usuários.
+ */
+
 public interface Seguranca {
 
+    /**
+     * Autentica o usuário. Cada classe é responsável por implementar seu próprio método de
+     * autenticação, pois ele pode variar.
+     *
+     * @param senha Senha passada pelo usuario.
+     * @return Retorna verdadeiro ou falso, dependendo da forma que a autenticação foi implementada.
+     */
     boolean autenticarSenha(String senha);
 
 }
