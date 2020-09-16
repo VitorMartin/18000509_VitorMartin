a880bc640a94efc48796d452bd64811071343fb5
VitorMartin <vitor.martin.simoni@gmail.com>
2020-09-16 16:02:42 -0300

    Criando clase App

--- Atividades/Atividade3/projeto/src/br/maua/Main.java
+++ Atividades/Atividade3/projeto/src/br/maua/Main.java
@@ -1,25 +1,10 @@
 package br.maua;
 
-import java.sql.*;
+import br.maua.models.App;
 
 public class Main {
 	public static void main(String[] args) {
-		try {
-			Connection con = DriverManager.getConnection("jdbc:sqlite:res/otakuDB.db");
-			Statement st = con.createStatement();
-			ResultSet resultSet = st.executeQuery("SELECT * FROM animes");
-			ResultSetMetaData rsmd = resultSet.getMetaData();
-			int columnsNumber = rsmd.getColumnCount();
-			while (resultSet.next()) {
-				for (int i = 1; i <= columnsNumber; i++) {
-					if (i > 1) System.out.print(",  ");
-					String columnValue = resultSet.getString(i);
-					System.out.print(rsmd.getColumnName(i) + " : " + columnValue);
-				}
-				System.out.println("");
-			}
-		} catch (SQLException throwables) {
-			throwables.printStackTrace();
-		}
+		App app = new App();
+		app.run();
 	}
 }
---
+++ Atividades/Atividade3/projeto/src/br/maua/models/App.java
@@ -0,0 +1,9 @@
+package br.maua.models;
+
+import java.util.Scanner;
+
+public class App {
+	public void run(){
+		
+	}
+}
