923c4b2cf8f7c642982245cff7c1d9a89133d917
VitorMartin <vitor.martin.simoni@gmail.com>
2020-09-16 16:38:05 -0300

    Implementando escolhas do usuario (anime ou manga e titulo)

--- Atividades/Atividade3/projeto/src/br/maua/models/App.java
+++ Atividades/Atividade3/projeto/src/br/maua/models/App.java
@@ -5,15 +5,39 @@ import java.util.Scanner;
 public class App {
 	public void run(){
 		Scanner sc = new Scanner(System.in);
-		int inp = -1;
+		int inp = Menu.INVALIDO;
+		String titulo = "";
 
 		Menu.saudacao();
 
 		while (inp != Menu.SAIR){
 			Menu.inicio();
-			inp = sc.nextInt();
-		}
 
-		Menu.despedida();
+			try {
+				inp = Integer.parseInt(sc.next());
+			}catch (Exception e){
+				inp = Menu.INVALIDO;
+			}
+
+			switch (inp){
+				case Menu.ANIME:
+					Menu.anime();
+					titulo = sc.next();
+					break;
+				case Menu.MANGA:
+					Menu.manga();
+					titulo = sc.next();
+					break;
+				case Menu.SAIR:
+					Menu.despedida();
+					break;
+				case Menu.INVALIDO:
+					Menu.numeroDesconhecido();
+					break;
+				default:
+					Menu.opcaoInvalida();
+					break;
+			}
+		}
 	}
 }
--- Atividades/Atividade3/projeto/src/br/maua/models/Menu.java
+++ Atividades/Atividade3/projeto/src/br/maua/models/Menu.java
@@ -1,13 +1,13 @@
 package br.maua.models;
 
 public class Menu {
-	public static int ANIME = 1;
-	public static int MANGA = 2;
-	public static int SAIR = 9;
+	public static final int ANIME = 1;
+	public static final int MANGA = 2;
+	public static final int SAIR = 9;
+	public static final int INVALIDO = -1;
 
 	public static void inicio (){
 		System.out.println(
-				"\n" +
 				"---------\n" +
 				ANIME + " - Anime" + "\n" +
 				MANGA + " - Manga"	+ "\n" +
@@ -18,10 +18,26 @@ public class Menu {
 	}
 
 	public static void saudacao(){
-		System.out.println("\n" + "〜".repeat(37) + "\n" + "Seja bem-vindo ao OtakuDB! Escolha uma das opcoes a seguir:\n" + "〜".repeat(37) + "\n");
+		System.out.println("〜".repeat(37) + "\n" + "Seja bem-vindo ao OtakuDB! Escolha uma das opcoes a seguir:\n" + "〜".repeat(37));
 	}
 
 	public static void despedida(){
-		System.out.println("\n" + "〜".repeat(29) + "\n" + "Obrigado por utilizar o OtakuDB, volte sempre!\nArigato go sai masu! (*＾▽＾)／" + "\n" + "〜".repeat(29));
+		System.out.println("〜".repeat(29) + "\n" + "Obrigado por utilizar o OtakuDB, volte sempre!\nArigato go sai masu! (*＾▽＾)／" + "\n" + "〜".repeat(29));
+	}
+
+	public static void numeroDesconhecido(){
+		System.out.println("Numero desconhecido");
+	}
+
+	public static void anime(){
+		System.out.println("Digite o titulo do seu Anime : ");
+	}
+
+	public static void manga(){
+		System.out.println("Digite o titulo do se Manga : ");
+	}
+
+	public static void opcaoInvalida(){
+		System.out.println("Opcao invalida.");
 	}
 }
