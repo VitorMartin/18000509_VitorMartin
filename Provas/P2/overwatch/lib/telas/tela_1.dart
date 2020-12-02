bfdd320dfb23483b2779d8b88c921d006518b747
VitorMartin <vitor.martin.simoni@gmail.com>
2020-12-02 17:41:05 -0300

    Logica das telas

--- Provas/P2/overwatch/lib/telas/tela_1.dart
+++ Provas/P2/overwatch/lib/telas/tela_1.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:overwatch/models/modelo_ow.dart';
+import 'package:overwatch/telas/tela_2.dart';
+import 'package:overwatch/utilities/network_helper.dart';
 
 class Tela1 extends StatefulWidget {
   final String title;
@@ -12,11 +15,71 @@ class Tela1 extends StatefulWidget {
 class _Tela1 extends State<Tela1> {
   final controlador = TextEditingController();
 
+  ModeloOw dados_modeloow = new ModeloOw();
+  CompetitiveStats dados_compstats = new CompetitiveStats();
+  CareerStats dados_careerstats = new CareerStats();
+  AllHeroes dados_allheroes = new AllHeroes();
+  Assists dados_assists = new Assists();
+  Best dados_best = new Best();
+  Combat dados_combat = new Combat();
+  Game dados_game = new Game();
+  Games dados_games = new Games();
+  Ratings dados_ratings = new Ratings();
+
+  Future fetchImagem() async {
+    var requisicao = NetworkHelper(
+        url: 'https://ow-api.com/v1/stats/pc/us/${controlador.text}/complete');
+    dados_modeloow = ModeloOw.fromJson(await requisicao.getData());
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(),
+        body: Column(
+          children: [
+            SizedBox(
+                width: 300,
+                height: 300,
+                child: Image.network(
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Overwatch_circle_logo.svg/1200px-Overwatch_circle_logo.svg.png")),
+            //
+            TextField(
+              controller: controlador,
+              decoration: InputDecoration(
+                  hintText: "Formato: User-1234 ",
+                  labelText: "Informe sua batlletag:",
+                  icon: Icon(Icons.drive_file_rename_outline)),
+            ),
+            //
+            RaisedButton(
+                color: Colors.grey[500],
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    side: BorderSide(color: Colors.black)),
+                child: Text('Mostrar rank'),
+                textColor: Colors.white,
+                onPressed: () async {
+                  await fetchImagem();
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => Tela2(
+                              dadosModeloow: dados_modeloow,
+                              dadosCompstats: dados_compstats,
+                              dadosCareerstats: dados_careerstats,
+                              dadoAllheroes: dados_allheroes,
+                              dadosAssists: dados_assists,
+                              dadosBest: dados_best,
+                              dadosCombat: dados_combat,
+                              dadosGame: dados_game,
+                              dadosGames: dados_games,
+                              dadosRatings: dados_ratings,
+                            )),
+                  );
+                }),
+          ],
+        ),
       ),
     );
   }
--- Provas/P2/overwatch/lib/telas/tela_2.dart
+++ Provas/P2/overwatch/lib/telas/tela_2.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:overwatch/models/modelo_ow.dart';
 import 'package:flutter/cupertino.dart';
 
 class Tela2 extends StatefulWidget {
+  final ModeloOw dadosModeloow;
+  final CompetitiveStats dadosCompstats;
+  final CareerStats dadosCareerstats;
+  final AllHeroes dadoAllheroes;
+  final Assists dadosAssists;
+  final Best dadosBest;
+  final Combat dadosCombat;
+  final Game dadosGame;
+  final Games dadosGames;
+  final Ratings dadosRatings;
+
+  const Tela2({
+    Key key,
+    @required this.dadosModeloow,
+    @required this.dadosCompstats,
+    @required this.dadosCareerstats,
+    @required this.dadoAllheroes,
+    @required this.dadosAssists,
+    @required this.dadosBest,
+    @required this.dadosCombat,
+    @required this.dadosGame,
+    @required this.dadosGames,
+    @required this.dadosRatings,
+  }) : super(key: key);
   @override
   _Tela2State createState() => _Tela2State();
 }
@@ -11,6 +36,29 @@ class _Tela2State extends State<Tela2> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "               Seu Rank é !! ",
+          ),
+          backgroundColor: Colors.orange[300],
+        ),
+        body: Column(
+          children: [
+            //
+            Container(
+              child: Image.network(
+                  'http://8gmwp015fo-flywheel.netdna-ssl.com/wp-content/uploads/sites/10/2017/10/blizzconbanners-ow.png'),
+            ),
+            Container(
+              child: Image.network(widget.dadosModeloow.ratingIcon),
+            ),
+            Container(
+              child: Text("Seu Sr é:" + widget.dadosModeloow.rating.toString()),
+            )
+
+            //
+          ],
+        ));
   }
 }
