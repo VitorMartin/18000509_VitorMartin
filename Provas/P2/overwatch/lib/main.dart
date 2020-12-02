b1ab94f2cba7feeef6e686caea123db3ea30ed44
VitorMartin <vitor.martin.simoni@gmail.com>
2020-12-02 17:37:11 -0300

    Main rotas

--- Provas/P2/overwatch/lib/main.dart
+++ Provas/P2/overwatch/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:overwatch/telas/tela_1.dart';
+import 'package:overwatch/telas/tela_2.dart';
 
 void main() {
   runApp(
-    MaterialApp(),
+    MaterialApp(
+      initialRoute: 'Page1',
+      routes: {
+        'Page1': (context) => Tela1(),
+        'Page2': (context) => Tela2(),
+      },
+    ),
   );
 }
